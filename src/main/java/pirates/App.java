81a243e979270f08ac75f83ca9fbaa80b0cbd281
priyanka-6209 <priyankapriyadarshini422@gmail.com>
2019-02-13 19:00:58 +0530

    First project!!

---
+++ src/main/java/pirates/App.java
@@ -0,0 +1,94 @@
+package pirates;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.net.*;
+import java.io.*;
+import java.util.regex.*;
+
+import com.opencsv.CSVReader;
+
+import java.util.*;
+//import au.com.bytecode.opencsv.*;
+
+public class App {
+
+	public static final String csvFile = "data.csv";
+	
+    public static void main(String[] args) {
+
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        String url = "jdbc:mysql://192.168.10.10:3306/datafeed";
+        String user = "root";
+        String password = "password";
+		int count = 0;
+		int totalcount = 0;
+		String values = "(0,0,0,0)";
+		String query = "";
+        try {
+		
+			CSVReader reader = new CSVReader(new FileReader(csvFile));
+			String[] nextLine;
+			String[] firstLine = reader.readNext();
+			
+			con = DriverManager.getConnection(url, user, password);            
+			st = con.createStatement();
+			
+			while ((nextLine = reader.readNext()) != null) {
+				
+				values += "("+ nextLine[0]+","+nextLine[1]+","+nextLine[2]+","+nextLine[3]+")";
+				
+				count++;
+				totalcount++;
+				
+				//if (count == 100) {
+					query  = "INSERT INTO  datafeed.raw_data ("
+						+"linenumber ,"
+						+"operation ,"
+						+"applicaion ,"
+						+"reportdate) "
+						+"VALUES " + values + ";";
+					
+					
+					
+					String query2 = "insert into raw_data (linenumber ,operation ,applicaion ,reportdate) values ('"+nextLine[0]+"','"+nextLine[1]+"','"+nextLine[2]+"','"+nextLine[3]+"');";
+					st.executeUpdate(query2);
+					count = 0;
+					values = "(0,0,0,0)";
+				//}
+			}
+			//query  = "DELETE FROM `raw_data` WHERE `TRIP_NO` = '0'";
+			//st.executeUpdate(query);
+			
+        } catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(App.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+		
+        } catch(Exception e){
+			e.printStackTrace();
+		} finally {
+            try {
+                if (st != null) {
+                    st.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+
+            } catch (SQLException ex) {
+                Logger lgr = Logger.getLogger(App.class.getName());
+                lgr.log(Level.WARNING, ex.getMessage(), ex);
+            }
+        }
+		System.out.println(totalcount);
+    }
+}
---
+++ src/main/java/pirates/Test.java
@@ -0,0 +1,9 @@
+package pirates;
+
+public class Test {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hi");
+	}
+}
